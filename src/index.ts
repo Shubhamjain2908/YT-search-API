7731f46143be7f535464c782ebe09257ef3d5501
Shubham Jain <shubham.jain@insider.in>
2023-01-07 20:11:01 +0530

    Dockerize the app & added error handlers

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-console.log("Hello world 2!");
+import mongoose from "mongoose";
+import { app } from "./app";
+
+const start = async () => {
+	console.log("YT service starting...");
+
+	if (!process.env.API_KEY) {
+		throw new Error("API_KEY must be defined");
+	}
+	if (!process.env.MONGO_URI) {
+		throw new Error("MONGO_URI must be defined");
+	}
+	if (!process.env.REDIS_URI) {
+		throw new Error("REDIS_URI must be defined");
+	}
+
+	try {
+		await mongoose.connect(process.env.MONGO_URI);
+		console.log("Connected to MongoDB!!!");
+	} catch (err) {
+		console.error("Startup error => ", err);
+	}
+
+	app.listen(3000, () => {
+		console.log("YT Service: Listening on port 3000!");
+	});
+};
+
+start();
